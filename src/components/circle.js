d8356084ea599f5abe9d7bf86805e56e049075c5
HariKishore7 <harikishoret99@gmail.com>
2020-11-06 15:34:28 +0530

    Implemented Successfully

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,14 @@
 
 
 // export class using module.exports
+const Shape=require('./shape');
+class Circle extends Shape{
+    constructor(){
+        super();
+    }
+    calculateArea(){
+        console.log("Area"+this.color);
+        return 3.14;
+    }
+}
+module.exports=Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
 
 // export class using module.exports
+class Shape{
+    constructor(){
+        this.color="yellow";
+    }
+    drawShape(){
+        console.log("here");
+    }
+    calculateArea(){
+        console.log("here");
+    }
+}
+module.exports=Shape;
